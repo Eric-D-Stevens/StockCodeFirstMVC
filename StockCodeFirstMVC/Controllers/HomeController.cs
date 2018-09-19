a26156276ff9ebd2b5fef12bbe48e77fa38b634b
Eric Stevens <ericstevens26101@gmail.com>
2018-09-19 01:35:11 -0700

    Initial commit

---
+++ StockCodeFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using StockCodeFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockCodeFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddUser()
+        {
+            ViewBag.Message = "Add User";
+
+            using (var db = new StockContext())
+            {
+                var UsersList = db.Users.ToList();
+                return View(UsersList);
+            }
+        }
+
+        public ActionResult AddUserButton(string userName)
+        {
+            using (var ctx = new StockContext())
+            {
+                var usr = new User { Name = userName };
+                ctx.Users.Add(usr);
+                ctx.SaveChanges();
+            }
+            return RedirectToAction("AddUser");
+        }
+
+
+
+
+        public ActionResult AddOrder()
+        {
+            using (var db = new StockContext())
+            {
+                var UsersList = db.Users.ToList();
+                return View(UsersList);
+            }
+        }
+
+        public ActionResult AddOrderButton(string userId, string symbol, string quantity, string pricePaid)
+        {
+            using (var ctx = new StockContext())
+            {
+                int theUserId = int.Parse(userId);
+
+                var userQuery = from b in ctx.Users
+                           where b.UserId.Equals(theUserId)
+                           select b;
+
+                var usr = userQuery.ToList()[0];
+
+                var ordr = new Order
+                {
+                    UserId = usr.UserId,
+                    UserName = usr.Name,
+                    Symbol = symbol,
+                    Quantity = int.Parse(quantity),
+                    PricePaid = double.Parse(pricePaid),
+                    DateAndTime = DateTime.Now
+                };
+
+                ctx.Orders.Add(ordr);
+                ctx.SaveChanges();
+            }
+
+            return RedirectToAction("AddOrder");
+        }
+
+    }
+}
---
+++ StockCodeFirstMVC/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace StockCodeFirstMVC.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Symbol { get; set; }
+        public int Quantity { get; set; }
+        public double PricePaid { get; set; }
+        public DateTime DateAndTime { get; set; }
+    }
+
+    public class StockContext : DbContext
+    {
+        public StockContext() : base("StockCodeFirstMVC")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
